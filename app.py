3938797cbfe2b3e9a1373412a0b51c9c45b9a72a
pratikreddy9 <pratik.sr@ayotta.com>
2024-12-22 10:20:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,9 +192,8 @@ def main():
     with col2:
         st.metric(label="Total Job Descriptions", value=total_jds)
 
-    # Uncomment the following lines to show duplicate count
-      total_duplicates = find_duplicate_resumes()
-      st.write(f"Number of duplicate resumes found: {total_duplicates}")
+    total_duplicates = find_duplicate_resumes()
+    st.write(f"Number of duplicate resumes found: {total_duplicates}")
 
     st.markdown("</div>", unsafe_allow_html=True)
 
